e2856895b68b7a25e71a9f5076a9643cb6200837
Mike Hagerty <mike.hagerty@heapanalytics.com>
2022-10-18 16:49:09 -0400

    All Files 1st commit (other than README)

---
+++ src/service/CustomerService.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+export class CustomerService {
+    getCustomersMedium() {
+        return axios.get('assets/demo/data/customers-medium.json')
+            .then(res => res.data.data);
+    }
+
+    getCustomersLarge() {
+        return axios.get('assets/demo/data/customers-large.json')
+                .then(res => res.data.data);
+    }
+
+    getCustomersChecking() {
+        return axios.get('assets/demo/data/customers-checking.json')
+            .then(res => res.data.data);
+    }
+
+    getCustomersSavings() {
+        return axios.get('assets/demo/data/customers-savings.json')
+            .then(res => res.data.data);
+    }
+
+
+}
