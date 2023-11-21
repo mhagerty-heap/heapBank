8fe327859a4fbc0f59a2be20b39bbee944b2361c
Mike Hagerty <mike.hagerty@heapanalytics.com>
2023-11-21 11:42:09 -0500

    added id's to dashboard images for rageclicks reports

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -191,7 +191,7 @@ const Dashboard = (props) => {
                             <div className="text-900 font-medium text-xl" data-heap-redact-text data-heaptextredactionexample>152</div>
                         </div>
                         <div className="flex align-items-center justify-content-center bg-blue-100 border-round" style={{width: '2.5rem', height: '2.5rem'}}>
-                            <i className="pi pi-shopping-cart text-blue-500 text-xl"/>
+                            <i id="dashboardShoppingCartImage" className="pi pi-shopping-cart text-blue-500 text-xl"/>
                         </div>
                     </div>
                     <span className="text-green-500 font-medium">24 new </span>
@@ -206,7 +206,7 @@ const Dashboard = (props) => {
                             <div className="text-900 font-medium text-xl" data-heap-redact-text data-heaptextredactionexample>$5678.90</div>
                         </div>
                         <div className="flex align-items-center justify-content-center bg-orange-100 border-round" style={{width: '2.5rem', height: '2.5rem'}}>
-                            <i className="pi pi-map-marker text-orange-500 text-xl"/>
+                            <i id="dashboardMapMarkerImage" className="pi pi-map-marker text-orange-500 text-xl"/>
                         </div>
                     </div>
                     <span className="text-green-500 font-medium">+52% </span>
@@ -221,7 +221,7 @@ const Dashboard = (props) => {
                             <div className="text-900 font-medium text-xl" data-heap-redact-text data-heaptextredactionexample>$1234.56</div>
                         </div>
                         <div className="flex align-items-center justify-content-center bg-cyan-100 border-round" style={{width: '2.5rem', height: '2.5rem'}}>
-                            <i className="pi pi-inbox text-cyan-500 text-xl"/>
+                            <i id="dashboardInboxImage" className="pi pi-inbox text-cyan-500 text-xl"/>
                         </div>
                     </div>
                     <span className="text-green-500 font-medium">+24%</span>
@@ -238,7 +238,7 @@ const Dashboard = (props) => {
                             </div>
                         </div>
                         <div className="flex align-items-center justify-content-center bg-purple-100 border-round" style={{width: '2.5rem', height: '2.5rem'}}>
-                            <i className="pi pi-comment text-purple-500 text-xl"/>
+                            <i id="dashboardTextBubbleImage" className="pi pi-comment text-purple-500 text-xl"/>
                         </div>
                     </div>
 
